d6ed9bb6bc42952fee755948fac2fce4b640dc83
nakashimashin <nakashimashin0517@gmail.com>
2023-09-14 23:24:23 +0900

    Linkコンポーネント作成

--- src/components/pages/admin-pages/Login.vue
+++ src/components/pages/admin-pages/Login.vue
@@ -1,6 +1,33 @@
+<script setup>
+</script>
+
 <template>
-    <div>Login</div>
+    <div id="flex-main" class="content">
+        <div class="title">hhhhhhh</div>
+    </div>
 </template>
 
 <style scoped>
+#flex-main{
+    background-color: var(--main-red);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 7%;
+}
+.content {
+    min-width: 100vw;
+}
+.title {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 500px;
+    width: 40%; 
+    font-size: 50px;
+    background: #FFFFFE;
+    border-radius: 0.25rem;
+}
+
 </style>
