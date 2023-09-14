fe142f611c98be78d418cbb3ceb480cf308ab54e
sakurrr <sakratliey11@gmail.com>
2023-09-15 02:55:43 +0900

    login画面作成

---
+++ src/components/components/Input.vue
@@ -0,0 +1,22 @@
+<script setup>
+    const props = defineProps({
+    title: String,
+  });
+</script>
+
+<template>
+<div>
+    <div>{{ props.title }}</div>
+    <input type="password" class="input-field"/>
+</div>
+</template>
+
+<style scoped>
+.input-field{
+    font-size:  20px;
+    width: 210%;
+    height: 50px;
+    outline: 1px solid black;
+    border-radius: 0.25rem;
+}
+</style>
