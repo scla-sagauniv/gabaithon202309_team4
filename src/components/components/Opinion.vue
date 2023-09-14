f3827562244e2184c1f5d899d1fb83d5e7e4db59
nakashimashin <nakashimashin0517@gmail.com>
2023-09-15 01:45:53 +0900

    Opinion用のコンポーネント作成

---
+++ src/components/components/Opinion.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+    <div class="opinion-box">意見</div>
+</template>
+
+<style scoped>
+.opinion-box {
+    height: 400px;
+    width: 40%; 
+    font-size: 50px;
+    background: #FFFFFE;
+    border-radius: 0.25rem;
+}
+
+</style>
--- src/components/pages/admin-pages/Read.vue
+++ src/components/pages/admin-pages/Read.vue
@@ -1,3 +1,32 @@
+<script setup>
+import Opinion from "../../components/Opinion.vue"
+</script>
+
 <template>
-    <div>Read</div>
+    <div id="flex-main" class="content">
+        <div id="flex-opinion" class="content-opinion">
+            <Opinion />
+            <Opinion />
+        </div>
+    </div>
 </template>
+
+<style scoped>
+#flex-main{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 7%;
+}
+#flex-opinion{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
+.content {
+    min-width: 100vw;
+}
+.content-opinion{
+    width: 80%;
+}
+</style>
